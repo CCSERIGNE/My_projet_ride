044881078382f223c33c8a28d2dcb3f652f91d77
mbayecracker <serigne@altacapital.com.br>
2021-01-02 16:06:40 -0300

    push project

---
+++ js/home.js
@@ -0,0 +1,176 @@
+$(document).ready(function (){
+alert("bem vindo")
+
+$("#listag").append(' <div class="col-md-12 col-sm-12 col-xs-12">
+                      <article class="ride-box clearfix">
+                    <div class="ride-content">
+                      <h3><a href="#">De Ibirubá-RS para Passo Fundo-RS</a></h3>passeio por <a href="#">Serigne Khassim Mbaye</a>
+                    </div>
+                                                         <ul class="ride-meta">
+
+                                                             <li class="ride-date">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
+                                                                     <i class="fa fa-calendar"></i>
+                                                                     July 20, 2014 at 19:00 PM
+                                                                 </a>
+                                                             </li><!-- end .ride-date -->
+
+                                                             <li class="ride-people">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
+                                                                     <i class="fa fa-user"></i>
+                                                                     1
+                                                                 </a>
+                                                             </li><!-- end .ride-people -->
+
+                                                             <li>
+                                                                 <a href="#">
+                                                                     <i class="fa fa-file"></i>
+                                                                     Consulte Mais informação
+                                                                 </a>
+                                                             </li>
+
+                                                         </ul><!-- end .ride-meta -->
+
+                                                     </article><!-- end .ride-box -->
+
+                                                     <article class="ride-box clearfix">
+
+                                                         <div class="ride-content">
+                                                             <h3><a href="#">De Ibirubá-RS para Quinze Novembro-RS</a></h3>passeio por <a href="#">Masthias Weidman</a>
+                                                         </div>
+
+                                                         <ul class="ride-meta">
+
+                                                             <li class="ride-date">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
+                                                                     <i class="fa fa-calendar"></i>
+                                                                     July 18, 2014 at 06:00 AM
+                                                                 </a>
+                                                             </li><!-- end .ride-date -->
+
+                                                             <li class="ride-people">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
+                                                                     <i class="fa fa-user"></i>
+                                                                     4
+                                                                 </a>
+                                                             </li><!-- end .ride-people -->
+
+                                                             <li>
+                                                                 <a href="#">
+                                                                     <i class="fa fa-file"></i>
+                                                                     Consulte Mais informação
+                                                                 </a>
+                                                             </li>
+
+                                                         </ul><!-- end .ride-meta -->
+
+                                                     </article><!-- end .ride-box -->
+
+                                                     <article class="ride-box clearfix">
+
+                                                         <div class="ride-content">
+                                                             <h3><a href="#">De Ibirubá-RS para Cachoueira-RS</a></h3>passeio por <a href="#">Ismael </a>
+                                                         </div>
+
+                                                         <ul class="ride-meta">
+
+                                                             <li class="ride-date">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
+                                                                     <i class="fa fa-calendar"></i>
+                                                                     July 15, 2014 at 20:00 PM
+                                                                 </a>
+                                                             </li><!-- end .ride-date -->
+
+                                                             <li class="ride-people">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
+                                                                     <i class="fa fa-user"></i>
+                                                                     3
+                                                                 </a>
+                                                             </li><!-- end .ride-people -->
+
+                                                             <li>
+                                                                 <a href="#">
+                                                                     <i class="fa fa-file"></i>
+                                                                     Consulte Mais informação
+                                                                 </a>
+                                                             </li>
+
+                                                         </ul><!-- end .ride-meta -->
+
+                                                     </article><!-- end .ride-box -->
+
+                                                     <article class="ride-box clearfix">
+
+                                                         <div class="ride-content">
+                                                             <h3><a href="#">De Ibirubá-RS para Santa Maria-RS</a></h3>passeio por <a href="#">Thiago Rocha</a>
+                                                         </div>
+
+                                                         <ul class="ride-meta">
+
+                                                             <li class="ride-date">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
+                                                                     <i class="fa fa-calendar"></i>
+                                                                     July 10, 2014 at 09:00 AM
+                                                                 </a>
+                                                             </li><!-- end .ride-date -->
+
+                                                             <li class="ride-people">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
+                                                                     <i class="fa fa-user"></i>
+                                                                     2
+                                                                 </a>
+                                                             </li><!-- end .ride-people -->
+
+                                                             <li>
+                                                                 <a href="#">
+                                                                     <i class="fa fa-file"></i>
+                                                                     Consulte Mais informação
+                                                                 </a>
+                                                             </li>
+
+                                                         </ul><!-- end .ride-meta -->
+
+                                                     </article><!-- end .ride-box -->
+
+                                                     <article class="ride-box clearfix">
+
+                                                         <div class="ride-content">
+                                                             <h3><a href="#">De Ibirubá-RS para Soledade-RS</a></h3>passeio por <a href="#">Edimar Monica</a>
+                                                         </div>
+
+                                                         <ul class="ride-meta">
+
+                                                             <li class="ride-date">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
+                                                                     <i class="fa fa-calendar"></i>
+                                                                     July 08, 2014 at 22:00 PM
+                                                                 </a>
+                                                             </li><!-- end .ride-date -->
+
+                                                             <li class="ride-people">
+                                                                 <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
+                                                                     <i class="fa fa-user"></i>
+                                                                     1
+                                                                 </a>
+                                                             </li><!-- end .ride-people -->
+
+                                                             <li>
+                                                                 <a href="#">
+                                                                     <i class="fa fa-file"></i>
+                                                                     Consulte Mais informação
+                                                                 </a>
+                                                             </li>
+
+                                                         </ul><!-- end .ride-meta -->
+
+                                                     </article><!-- end .ride-box -->
+
+                                                     <div class="clearfix"></div>
+
+                                                     <div class="read-all pagination-margin">
+                                                         <a href="rides.html">Leia tudo</a>
+                                                     </div>
+
+                                                 </div><!-- end .col-md-12 col-sm-12 col-xs-12 -->
+')
+})
